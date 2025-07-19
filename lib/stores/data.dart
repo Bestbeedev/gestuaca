bc5e40cb184f3d19d1c71b2c77c97a8d72ccc63f
Bestbeedev <josueaoga0@gmail.com>
2025-07-19 11:45:22 +0100

    Built:BottomNavigationBar Curved Customization

---
+++ lib/stores/data.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+//Navigation bar icons for the app -Director
+
+final List<IconData> iconsDirector = [
+  Icons.home_outlined,
+  Icons.people_alt_outlined,
+  Icons.people_outline_outlined,
+  Icons.settings_outlined,
+];
+
+final List<IconData> activeIconsDirector = [
+  Icons.home,
+  Icons.people_alt,
+  Icons.book,
+  Icons.settings,
+];
+
+// Navigation bar icons for the app -Teachers
+
+final List<IconData> iconsTeachers = [
+  Icons.dashboard_outlined,
+  Icons.people_alt_outlined,
+  Icons.book_online_outlined,
+  Icons.calendar_today_outlined,
+];
+
+final List<IconData> activeIconsTeachers = [
+  Icons.dashboard,
+  Icons.people_alt,
+  Icons.book,
+  Icons.calendar_today,
+];
+
+// Navigation bar icons for the app -Students
+
+final List<IconData> iconsStudents = [
+  Icons.dashboard_outlined,
+  Icons.book_online_outlined,
+  Icons.calendar_today_outlined,
+];
+
+final List<IconData> activeIconsStudents = [
+  Icons.dashboard,
+  Icons.book,
+  Icons.calendar_today,
+];
+
