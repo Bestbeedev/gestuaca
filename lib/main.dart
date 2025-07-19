60079cad365c44e5fcac6f68f12fb53fede1a1f7
Bestbeedev <josueaoga0@gmail.com>
2025-07-19 11:49:26 +0100

    Setup:Routes and trying to implements Redirection if auth==true

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,19 @@ import 'package:gestuaca/screens/home_page.dart';
 import 'package:gestuaca/screens/panels/directeur/dashboard.dart';
 import 'onboarding/onboard_layout.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '/stores/states.dart';
+import 'package:gestuaca/screens/panels/students/dashboard.dart';
+import 'package:gestuaca/screens/panels/teachers/dashboard.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,12 +34,12 @@ class MyApp extends StatelessWidget {
         '/home': (context) => const HomePage(),
         '/auth/signup': (context) => const SignupPage(),
         '/auth/login': (context) => const LoginPage(),
-        '/panels/directeur/dashboard': (context) => const DashboardPage()
+        '/panels/directeur/dashboard': (context) => const DashboardPage(),
+        '/panels/students/dashboard':(context)=>const DashboardStudents(),
+        'panels/teachers/dashboard':(context)=>const DashboardTeachers()
       },
     );
   }
 }
-
-
 
 
